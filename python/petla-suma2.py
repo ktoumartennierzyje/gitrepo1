1806fbcf77c71cbe19fa6116970ca3767f4691c8
ktoumartennierzyje <bartlomiej.kozera@lo1.sandomierz.pl>
2018-11-13 13:31:34 +0100

    petle for i while

---
+++ python/petla-suma2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  petla-suma.py
+
+
+def main(args):
+    
+    suma = 0
+    liczba = int(input("podaj liczbe: "))
+    while suma =< 75:
+        suma += liczba
+        print(suma)
+        liczba = int(input("podaj liczbe: "))
+                                                                                 
+    print()
+    print("suma liczb: ", suma)
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))

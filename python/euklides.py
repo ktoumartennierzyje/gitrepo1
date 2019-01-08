3cb27b54724f6434b0a50ad54f3f04b2ef75f4a8
ktoumartennierzyje <bartlomiej.kozera@lo1.sandomierz.pl>
2019-01-08 13:30:48 +0100

    silnia

--- python/euklides.py
+++ python/euklides.py
@@ -18,11 +18,17 @@ def nwdv2(a, b):
         a = a % b
         b = b - a
     return b
+    
+def testuj():
+    assert(nwdv1(20, 4)== 4)
+    assert(nwdv2(20, 4)== 4)
+    assert(nwdv2(11, 4)== 1)
 
 def main(args):
     a = int(input('Podaj liczbę a: '))
     b = int(input('Podaj liczbę b: '))
-    print(nwdv2(a, b))
+    #print(nwdv2(a, b))
+    testuj()
     return 0
 
 if __name__ == '__main__':

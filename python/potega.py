d099ae1c3330754479cf505684f7b3dcade06514
ktoumartennierzyje <bartlomiej.kozera@lo1.sandomierz.pl>
2019-02-18 12:37:17 +0100

    rekurencja

--- python/potega.py
+++ python/potega.py
@@ -14,13 +14,20 @@ def potega_it(a, n):
     for i in range(n):
         wynik = a * wynik
     return wynik
+    
+def potega_rek(a, n):
+    if n == 0:
+        return 1
+    if a == 0:
+        return 0
+    return potega_rek(a^(n-1)*a)
 
 def main(args):
-    assert potega_it(2, 0) == 1
-    assert potega_it(2, 1) == 2
-    assert potega_it(0, 2) == 0
-    assert potega_it(1, 2) == 1
-    assert potega_it(3, 3) == 27
+    assert potega_rek(2, 0) == 1
+    assert potega_rek(2, 1) == 2
+    assert potega_rek(0, 2) == 0
+    assert potega_rek(1, 2) == 1
+    assert potega_rek(3, 3) == 27
     
     a = input('Podaj podstawe : ')
     n = input('Podaj wykładnik: ')

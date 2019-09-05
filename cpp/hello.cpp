780677ae7f9ab88131d6d0870b9ef87be6f26f6d
ktoumartennierzyje <bartlomiej.kozera@lo1.sandomierz.pl>
2019-09-05 11:41:45 +0200

    C++ 1

--- cpp/hello.cpp
+++ cpp/hello.cpp
@@ -1,13 +1,17 @@
 /*
  * hello.cpp
-
+ *
+ * Copyright 2019  <>
+ */
 
 
 #include <iostream>
 
+using namespace std;
+
 int main(int argc, char **argv)
 {
-
+    cout << "Programuje w C++";
     return 0;
 }
 

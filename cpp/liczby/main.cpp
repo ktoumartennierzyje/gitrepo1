bcd4c6e6644b5673105386781ae262929e51d556
ktoumartennierzyje <bartlomiej.kozera@lo1.sandomierz.pl>
2019-09-23 10:35:13 +0200

    jeszcze wincy c++

--- cpp/liczby/main.cpp
+++ cpp/liczby/main.cpp
@@ -37,5 +37,39 @@ int main()
     {
         cout << c<<" "<<b<<" "<<a;
     }
+
+    cout<<endl;
+
+        if (a<=b && a<=c)
+    {
+        if (b<=c)
+        {
+            cout<<a<<" "<<b<<" "<<c;
+        }
+        else
+        {
+            cout<<a<<" "<<c<<" "<<b;
+        }
+    }
+    else if (b<=c && b<a)
+    {
+        if (a<=c)
+        {
+            cout << b<<" "<<a<<" "<<c;
+        }
+        else
+        {
+            cout << b <<" "<<c<<" "<<a;
+        }
+    }
+    else if (c<b && c<a && a<=b)
+    {
+        cout << c<<" "<<a<<" "<<b;
+    }
+    else
+    {
+        cout << c<<" "<<b<<" "<<a;
+    }
+
     return 0;
 }

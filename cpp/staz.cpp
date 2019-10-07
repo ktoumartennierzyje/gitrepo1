4c935cff044ab34867d9ba52144d2c5fe5aa2bbe
ktoumartennierzyje <bartlomiej.kozera@lo1.sandomierz.pl>
2019-10-07 10:36:14 +0200

    ćwiczenie

---
+++ cpp/staz.cpp
@@ -0,0 +1,31 @@
+/*
+ * staz.cpp
+ *
+ * Copyright 2019  <>
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+void awans (int &staz, int &zarobek)
+{
+
+}
+
+void (int staz, int zarobek)
+{
+    cout<<"Pracujesz "<<staz<<" lat  i zarabiasz "<<zarobek;
+}
+
+int main(int argc, char **argv)
+{
+    int staż = 1;
+    int zarobek = 1000;
+    int lata = 0;
+    cout << "Ile lat Pracujesz?"<<endl;
+    cin << lata;
+    return 0;
+}
+

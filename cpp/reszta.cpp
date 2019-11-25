c1732e493a25fc83a475da7950991735872d7e39
ktoumartennierzyje <bartlomiej.kozera@lo1.sandomierz.pl>
2019-11-25 10:35:57 +0100

    horner

---
+++ cpp/reszta.cpp
@@ -0,0 +1,18 @@
+/*
+ * reszta.cpp
+ *
+ * Copyright 2019  <>
+ */
+
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+
+    return 0;
+}
+R = 69
+R = 60 - 50*(69%50) = 19
+R = 19 -10 * (19%10) = 9

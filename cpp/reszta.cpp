289d88eda35357e4d0a6c85ed1927a9080c8b2ac
ktoumartennierzyje <bartlomiej.kozera@lo1.sandomierz.pl>
2019-11-28 11:40:43 +0100

    dokoncz program

--- cpp/reszta.cpp
+++ cpp/reszta.cpp
@@ -8,11 +8,61 @@
 #include <iostream>
 using namespace std;
 
+int nominaly[100];
+int LiczbaNominalow;
+
+void wczytajNominaly(){
+    cout<<"Podaj liczbe nominałów: ";
+    cin>>LiczbaNominalow;
+    cout<<"Podaj nominały w kolejności malejącej"<<endl;
+    int i = 0;
+    int nominal = 0;
+    nominaly[i] = nominal;
+    i++ ;
+    while(i<LiczbaNominalow){
+        cin>>nominal;
+        if(nominal>nominaly[i]){
+            cout<<"Nominał większy od poprzedniego!"<<endl;
+            continue;
+            }
+        nominaly[i] = nominal;
+        i++;
+    };
+
+    for(int i=0; i<LiczbaNominalow; i++){
+        cout<<nominaly[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int znajdzReszte(int reszta){
+    cout<<"Wpłacone nominały: "<<endl;
+    int aktualnyNominal = 0;
+    while(reszta>0 && aktualnyNominal<liczbaNominalow){
+        cout<<"reszta = "<<reszta<<endl;
+        znajdź największy dostepny nominał mniejszy od reszty
+        cout<<"aktualnyn nominał: "<<nominaly[aktualnyNominal];
+        oblicz wymaną liczbe nominałów
+        pomniejsz reszte
+    }
+}
+
 int main(int argc, char **argv)
 {
+    wczytajNominaly();
+    int cena;
+    int wplata;
+    cout<<"Podaj cene";
+    cin>>cena;
+
+    do{
+        cout<<"Podaj wpłatę: ";
+        cin>>wplata;
+    }while(wplata<ceny);
+
+    znajdzReszte(wplata - cena);
 
     return 0;
 }
-R = 69
-R = 60 - 50*(69%50) = 19
-R = 19 -10 * (19%10) = 9
+
+

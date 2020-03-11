2f4192987fc74eb9ea39af016339e393a3bb695d
ktoumartennierzyje <bartlomiej.kozera@lo1.sandomierz.pl>
2020-03-11 09:39:18 +0100

    kolka

---
+++ public_html/javascripts/p5/empty-example/sketch.js
@@ -0,0 +1,22 @@
+function setup() {
+  createCanvas(700, 600);
+}
+
+function draw() {
+  noFill();
+  strokeWeight(6);
+  stroke('blue');
+  ellipse(200, 200, 150, 150);
+  strokeWeight(6);
+  stroke('black');
+  ellipse(360, 200, 150, 150);
+  strokeWeight(6);
+  stroke('red');
+  ellipse(520, 200, 150, 150);
+  strokeWeight(6);
+  stroke('yellow');
+  ellipse(280, 300, 150, 150);
+  strokeWeight(6);
+  stroke('green');
+  ellipse(440, 300, 150, 150);
+}
